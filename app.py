a8f449530c1b78cfccdb578348adedcedcb0cc0e
tddev123 <154409493+tddev123@users.noreply.github.com>
2024-03-25 21:44:06 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,render_template, request
+from views import views
+import aitrainer
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, template_folder='templates')
+app.register_blueprint(views, url_prefix="/views")
+
+
+    
+
+    
+    
+
+    
+
+@app.route('/tutcalc', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        progression = request.form['tut']
+        
+        if progression == ("stronger"):
+            result = "same sets"
+        if progression == ("weaker"):
+            result = "less sets"
+        
+        return render_template('tutcalc.html', result = result)
+    
+    
+    
+    
+
+    
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
